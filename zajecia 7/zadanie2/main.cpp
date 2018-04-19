1d892142bb6cfdddd16d46883ba336a459716aa6
Hubert <32572794+hubertgutowski@users.noreply.github.com>
2018-04-19 15:56:16 +0200

    plopol

--- zajecia 7/zadanie2/main.cpp
+++ zajecia 7/zadanie2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+namespace poly {
 class Reguralar_polygon
 {
 private:
@@ -30,12 +31,20 @@ public:
     virtual void czystowirt()=0;
     Reguralar_polygon(float bok,int ilosc,int h) : bok(bok), ilosc(ilosc),wysokosc(h)
     {
-
         pole=(bok/2*tan(M_PI/ilosc))*bok*ilosc;
     }
 
 };
+class ReguralPiramid : public Reguralar_polygon
+{
 
+    void czystowirt();
+    float volume()
+    {
+        return this->getpole()*this->getwysokosc()/3;
+    }
+};
+}
 
 int main(int argc, char *argv[])
 {
